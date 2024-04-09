83ddc54a378d56a4268c02da98181390cb5162c9
YochevedRot <166374109+YochevedRot@users.noreply.github.com>
2024-04-09 15:21:31 +0300

    Add files via upload

---
+++ dbo.CitiesList.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[CitiesList] (
+    [name] NVARCHAR (450) DEFAULT (N'') NOT NULL,
+    [חיפה] NCHAR(10) NULL, 
+    CONSTRAINT [PK_CitiesList] PRIMARY KEY CLUSTERED ([name] ASC)
+);
+
